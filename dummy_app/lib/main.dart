7aaf0fe88397642c6cd09354e9d80261dbad49fc
Tansu Dasli <tansudasli@gmail.com>
2019-06-26 14:21:14 +0300

    Initialize - new dummy app to test from scratch

---
+++ dummy_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(
+      title: 'dummy app',
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState  extends State<MyHomePage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Title'),),
+    );
+  }
+}
+
